99973d9242bd4d61fafd1987a7f83510bb5f173b
Bnear8273 <Bnear8273@local>
2026-08-06 21:31:57 +0800

    Align convex builder declarations

--- src/he3d_convex_builder.hpp
+++ src/he3d_convex_builder.hpp
@@ -5,32 +5,8 @@
 namespace HE3D
 {
 
-struct ConvexBuildPart
-{
-   float3 vertices[64];
-   int32_t vertexCount;
-   float3 faceAxes[16];
-   int32_t faceAxisCount;
-   float3 edgeAxes[64];
-   int32_t edgeAxisCount;
-   AABB bounds;
-
-   ConvexBuildPart()
-       : vertices(), vertexCount(0), faceAxes(), faceAxisCount(0), edgeAxes(), edgeAxisCount(0),
-         bounds()
-   {
-   }
-};
-
-struct ConvexBuildData
-{
-   ConvexBuildMode mode;
-   ConvexBuildPart parts[16];
-   int32_t         partCount;
-   AABB            bounds;
-
-   ConvexBuildData() : mode(ConvexBuildMode::SingleHull), parts(), partCount(0), bounds() {}
-};
+struct ConvexBuildPart;
+struct ConvexBuildData;
 
 class ConvexBuilder
 {
